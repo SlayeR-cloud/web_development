eea2c75bf3f0f1968ffe459d366d742bf3fbae82
Ssaylem Murillo <83789210+SlayeR-cloud@users.noreply.github.com>
2022-03-30 09:29:35 -0500

    Adding exercise 3 SPA
    It is confirmed the single page application project uploading

---
+++ Myproject/app.js
@@ -0,0 +1,17 @@
+// saving data in the table
+
+var name;
+var lastName;
+var email;
+var phone;
+var address;
+var bornDate;
+var notes;
+
+var table = document.getElementById("table");
+var button = document.getElementById("input_button");
+var counter = 1;
+
+button.addEventListener("click", function(){
+    console.log("You clicked the button");
+})
